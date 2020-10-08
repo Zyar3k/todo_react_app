066a1a2057e8d91d7aa1405edbcd86b240bbb0d7
Arkadiusz Zygan <a.zygan@gmail.com>
2020-10-08 21:37:16 +0200

    Add funcionality to form-add task

--- src/components/AddTask/AddTask.js
+++ src/components/AddTask/AddTask.js
@@ -1,12 +1,88 @@
 import React from "react";
 
 class AddTask extends React.Component {
-  state = {};
+  minDate = new Date().toISOString().slice(0, 10);
+  state = {
+    text: "",
+    date: this.minDate,
+    importantCheck: false,
+    himportantCheck: false,
+  };
+
+  handleTextChange = (event) => {
+    this.setState({
+      text: event.target.value,
+    });
+  };
+
+  handleImportantChange = (event) => {
+    this.setState({
+      importantCheck: event.target.checked,
+    });
+  };
+  handleHiImportantChange = (event) => {
+    this.setState({
+      himportantCheck: event.target.checked,
+    });
+  };
+
+  handleDateChange = (event) => {
+    this.setState({
+      date: event.target.value,
+    });
+  };
+
+  handleClick = () => {
+    console.log("test");
+  };
   render() {
+    let maxDate = this.minDate.slice(0, 4) * 1 + 1;
+    maxDate = maxDate + "-12-31";
     return (
-      <>
-        <h2>AddTask</h2>
-      </>
+      <div>
+        <input
+          type="text"
+          placeholder="wpisz coś"
+          value={this.state.value}
+          onChange={this.handleTextChange}
+        />
+        <br />
+        <label htmlFor="important">
+          <input
+            type="checkbox"
+            name=""
+            id="important"
+            checked={this.state.importantCheck}
+            onChange={this.handleImportantChange}
+          />
+          Priorytet
+        </label>
+        <label htmlFor="highImportant">
+          <input
+            type="checkbox"
+            name=""
+            id="highImportant"
+            checked={this.state.himportantCheck}
+            onChange={this.handleHiImportantChange}
+          />
+          Wysoki priorytet
+        </label>
+        <br />
+        <label htmlFor="date">
+          Termin wykonania
+          <input
+            type="date"
+            name=""
+            min={this.minDate}
+            max={maxDate}
+            id="date"
+            value={this.state.date}
+            onChange={this.handleDateChange}
+          />
+        </label>
+        <br />
+        <button onClick={this.handleClick}>Add task</button>
+      </div>
     );
   }
 }
