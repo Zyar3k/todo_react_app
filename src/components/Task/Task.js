65b960562661b308d60bde4903812e6ecb10aae3
Arkadiusz Zygan <a.zygan@gmail.com>
2020-10-08 20:49:16 +0200

    Add important and high important styles

--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -4,18 +4,54 @@ import "./Task.scss";
 const Task = (props) => {
   // console.log(props);
 
-  const { text, date, id } = props.task;
+  const importantStyle = { color: "orange" };
+  const highImportantStyle = { color: "red" };
 
-  return (
-    <div className="taskBox">
-      <div className="text">{text}</div>
-      <div className="date">{date}</div>
-      <button onClick={() => props.change(id)}>Zrobione</button>
-      <button onClick={() => props.delete(id)} className="deleteBtn">
-        X
-      </button>
-    </div>
-  );
+  const {
+    text,
+    date,
+    id,
+    active,
+    important,
+    highImportant,
+    finishDate,
+  } = props.task;
+
+  if (active) {
+    return (
+      <div className="taskBox">
+        <div
+          className="text"
+          style={
+            important
+              ? importantStyle
+              : null || highImportant
+              ? highImportantStyle
+              : null
+          }
+        >
+          {text}
+        </div>
+        <div className="date">termin-{date}</div>
+        <button onClick={() => props.change(id)}>Zrobione</button>
+        <button onClick={() => props.delete(id)} className="deleteBtn">
+          X
+        </button>
+      </div>
+    );
+  } else {
+    const finishTask = new Date(finishDate).toLocaleString();
+    return (
+      <div className="taskBox">
+        <div className="text">{text}</div>
+        <div className="date">termin -{date}</div>
+        <div className="date">wykonano - {finishTask}</div>
+        <button onClick={() => props.delete(id)} className="deleteBtn">
+          X
+        </button>
+      </div>
+    );
+  }
 };
 
 export default Task;
