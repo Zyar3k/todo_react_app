5fd69b4d33aace93d70f8b315b7a8dddf2066c5e
Arkadiusz Zygan <a.zygan@gmail.com>
2020-10-08 20:27:59 +0200

    Show divided tasks- active & done

--- src/components/TaskList/TaskList.js
+++ src/components/TaskList/TaskList.js
@@ -1,9 +1,20 @@
 import React from "react";
 import Task from "../Task/Task";
+import "./TaskList.scss";
 
 const TaskList = (props) => {
   // console.log(props.tasks.length);
-  const tasks = props.tasks.map((task) => (
+  let activeTasks = props.tasks.filter((task) => task.active);
+  let doneTasks = props.tasks.filter((task) => !task.active);
+  activeTasks = activeTasks.map((task) => (
+    <Task
+      key={task.id}
+      task={task}
+      delete={props.delete}
+      change={props.change}
+    />
+  ));
+  doneTasks = doneTasks.map((task) => (
     <Task
       key={task.id}
       task={task}
@@ -14,11 +25,13 @@ const TaskList = (props) => {
   return (
     <div>
       <div className="activeTasks">
-        <h2>activ TaskList (0)</h2>
-        {tasks}
+        <h2>activ TaskList ({activeTasks.length})</h2>
+        {activeTasks.length > 0 ? activeTasks : <h1>0 tasks</h1>}
       </div>
       <div className="doneTasks">
-        <h2>done TaskList (0)</h2>
+        <h2>done TaskList ({doneTasks.length})</h2>
+        {doneTasks.length > 3 && <span>Only 3 last tasks</span>}
+        {doneTasks.slice(0, 3)} {/* ??? */}
       </div>
     </div>
   );
